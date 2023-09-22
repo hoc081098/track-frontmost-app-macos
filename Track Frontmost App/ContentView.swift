d32874233b35e5f1b7168360baaf3139659114a1
hoc081098@gmail.com <hoc081098@gmail.com>
2023-09-22 18:39:41 +0700

    init

---
+++ Track Frontmost App/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Track Frontmost App
+//
+//  Created by Hoc Nguyen T. on 9/22/23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Track Frontmost App/Track_Frontmost_AppApp.swift
@@ -0,0 +1,17 @@
+//
+//  Track_Frontmost_AppApp.swift
+//  Track Frontmost App
+//
+//  Created by Hoc Nguyen T. on 9/22/23.
+//
+
+import SwiftUI
+
+@main
+struct Track_Frontmost_AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
